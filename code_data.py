8396d7fadb531daa4866ea091ac8bd201462d811
Ubuntu <ubuntu@ip-172-31-24-27.us-east-2.compute.internal>
2020-01-24 05:12:15 +0000

    code_data

--- code_data.py
+++ code_data.py
@@ -2,6 +2,7 @@ import pandas as pd
 import numpy as np
 import os
 import argparse
+from sklearn.preprocessing import MinMaxScaler
 parser = argparse.ArgumentParser(description='This program takes in the output files of read_data.py, removes outliers,  and code the data')
 parser.add_argument('-i', '--inprefix', type=str, required=True, help="prefix for input files")
 parser.add_argument('-o', '--outprefix', type=str, required=True, help="prefix for output files")
@@ -31,36 +32,50 @@ for i in range(0,n):
     vardict[vardes.iloc[i,0]]=vardes.iloc[i,4]
 rawx=pd.read_csv(rawxfile)
 m=len(rawx.index)
-outlierrow=[0 for i in range(m)]
 dumvar=[]
 numvar=[]
 for colname in rawx.columns:
     if vardict[colname]=='c':
-        keepvar=1
-        if rmol:
-            outliers=[]
-            valuecounts=rawx[colname].value_counts()
-            for i in range(len(valuecounts.index)):
-                if valuecounts[i]/m<c_outlier_cut:
-                    outliers.append(valuecounts.index[i])
-            if len(valuecounts.index)-len(outliers)<2:
-                keepvar=0
-            else:
-                if len(outliers)>0:
-                    for j in range(m):
-                        if rawx[colname][j] in outliers:
-                            outlierrow[j]=1
-        if keepvar==1:
-            dumvar.append(colname)
-
+        dumvar.append(colname)
     if vardict[colname]=='n':
         numvar.append(colname)
-        if rmol:
-            q4=np.max(rawx[colname])
-            q3=np.quantile(rawx[colname],0.75)
-            q1=np.quantile(rawx[colname],0.25)
-            q0=np.min(rawx[colname])
-            print(str(q4)+"\t"+str(q3)+"\t"+str(q1)+"\t"+str(q0))
-print(dumvar)
-print(numvar)
+codedx=pd.get_dummies(rawx,columns=dumvar)
+if rmol:
+    keepfea=[]
+    for feaname in codedx.columns:
+        if feaname in numvar:
+            q4=np.max(codedx[feaname])
+            q3=np.quantile(codedx[feaname],0.75)
+            q1=np.quantile(codedx[feaname],0.25)
+            q0=np.min(codedx[feaname])
+            adjfea=codedx[feaname]
+            if q3-q1>0:
+                iqr=q3-q1
+                upperlimit=q3+iqr_magnitude*iqr
+                lowerlimit=q1-iqr_magnitude*iqr
+                for i in range(m):
+                    if codedx.loc[i,feaname]>upperlimit:
+                        codedx.loc[i,feaname]=upperlimit
+                    if codedx.loc[i,feaname]<lowerlimit:
+                        codedx.loc[i,feaname]=lowerlimit
+            keepfea.append(feaname)
+        else:
+            varstat=codedx[feaname].value_counts()
+            frac=varstat[0]/(varstat[0]+varstat[1])
+            if frac>c_outlier_cut and frac<1-c_outlier_cut:
+                keepfea.append(feaname)
+    codedx_ol_rmd=codedx[keepfea]
+else:
+    codedx_ol_rmd=codedx.copy()
+scaler = MinMaxScaler()
+scaler.fit(codedx_ol_rmd.to_numpy())
+codedx_final=pd.DataFrame(data=scaler.transform(codedx_ol_rmd.to_numpy()),columns=codedx_ol_rmd.columns)
+#print(codedx_final)
 
+rawy=pd.read_csv(rawyfile)
+if vardict[rawy.columns[0]]=='yc':
+    codedy=pd.get_dummies(rawy)
+else:
+    codedy=rawy.copy()
+codedy.to_csv(args.outprefix+".codedy",index=False)
+codedx_final.to_csv(args.outprefix+".codedx",index=False)
